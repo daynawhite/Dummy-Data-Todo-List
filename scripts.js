b742741d37be74a5dab6cad85ba5588bd42a52bf
daynawhite <dayna26.2@gmail.com>
2023-09-13 20:59:13 -0500

    created for loops and included data from json placeholder through fetch command

--- scripts.js
+++ scripts.js
@@ -1,33 +1,37 @@
 // fake array to begin with
-let arrayOfTodos = [
-    {
-    "userId": 14,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-},
-{
-    "userId": 20,
-    "id": 2,
-    "title": "delectus aut autem",
-    "completed": false
-}]
+// let arrayOfTodos = [
+//     {
+//     "userId": 14,
+//     "id": 1,
+//     "title": "delectus aut autem",
+//     "completed": false
+// },
+// {
+//     "userId": 20,
+//     "id": 2,
+//     "title": "something else",
+//     "completed": false
+// }]
 
-// const fetchTodos = () => {
-//     fetch('https://jsonplaceholder.typicode.com/todos')
-//     .then( (response) => response.json())
-//     .then( (json) => arrayOfTodos = json)
-// }
+const fetchTodos = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then( (response) => response.json())
+    .then( (json) => arrayOfTodos = json)
+}
 
 const logTodos = () => {
-    console.log(arrayOfTodos[0].title)
-   
+    for (let i = 0; i < arrayOfTodos.length; i++) {
+    console.log(arrayOfTodos[i].title)
+    }
 }
 
 let list = document.getElementById("todo-list") 
-let newListItem = document.createElement('LI')
+
 
 const populateTodos = () => {
-    newListItem.innerText = arrayOfTodos[0].title
-    list.appendChild(newListItem)
+    for (let i = 0; i < arrayOfTodos.length; i++) {
+        let newListItem = document.createElement('LI')
+        newListItem.innerText = arrayOfTodos[i].title
+        list.appendChild(newListItem)
+    }
 }
