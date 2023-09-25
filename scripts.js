df606aa84bc9de8bf056e9dfce8ead00e20abf3d
daynawhite <dayna26.2@gmail.com>
2023-09-25 13:08:08 -0500

    add buttons and JS for filtering by user ID and by completed or not

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
 
+const todoList = document.getElementById('todo-list')
+
+// const userIdInput = document.getElementById('userId').value
+// console.log(userIdInput)
 
 const fetchTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos')
@@ -8,30 +12,98 @@ const fetchTodos = () => {
 
 const logTodos = () => {
     for (let i = 0; i < arrayOfTodos.length; i++) {
-    console.log(arrayOfTodos[i].title)
+        // console.log(arrayOfTodos[i].title)
+        console.log(arrayOfTodos[i])
+
+    }
+
+    // console.log(arrayOfTodos)
+}
+
+// let list = document.getElementById("todo-list") 
+
+const populateTodos = () => {
+    clearTodos()
+    // let listArray = array.from(li)
+    // listArray.forEach(function(elem) {
+    // })
+
+    for (let i = 0; i < arrayOfTodos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = arrayOfTodos[i].title;
+       
+        todoList.appendChild(li)
     }
 }
 
-let list = document.getElementById("todo-list") 
+const filterTodosById = () => {
+    clearTodos()
+    const userIdInput = document.getElementById('userId').value
+
+    let filteredTodos = arrayOfTodos.filter((todo)=> todo.userId == userIdInput)
+    
+    for (let i = 0; i < filteredTodos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = filteredTodos[i].title;
+       
+        todoList.appendChild(li)
+    }
+    // console.log(filteredTodos)
+}
+
+const filterByComplete = () => {
+    clearTodos()
+    const userIdInput = document.getElementById('userId').value
+
+    let filteredTodos = arrayOfTodos.filter((todo)=> todo.userId == userIdInput)
+    for (let i = 0; i < filteredTodos.length; i++) {
+        if (filteredTodos[i].completed == true){
+            let li = document.createElement('li');
+            li.textContent = filteredTodos[i].title;
+           
+            todoList.appendChild(li)
+        }
+    }
+}
+const filterByIncomplete = () => {
+    clearTodos()
+    const userIdInput = document.getElementById('userId').value
+
+    let filteredTodos = arrayOfTodos.filter((todo)=> todo.userId == userIdInput)
+    for (let i = 0; i < filteredTodos.length; i++) {
+        if (filteredTodos[i].completed == false){
+            let li = document.createElement('li');
+            li.textContent = filteredTodos[i].title;
+           
+            todoList.appendChild(li)
+        }
+    }
+}
+
+const clearTodos = () => {
+    todoList.innerHTML = '';
+}
+// const saveIdInput = (num) => {
+//  idInput = parseInt(num) 
+// }
 
 
-const filterTodos = () => {
-    list.remove()
-    let userId = document.getElementById("userId")
-    const filteredList = arrayOfTodos.filter(index === userId)
-   
+// const filterTodosById = () => {
+//        todoList.innerHTML = '';
+// }
+
+
+//    const filteredList = arrayOfTodos.filter()
+//    for (let i = 0; i < arrayOfTodos.length; i++) {
+
+    // IF arrayOfTodos.userID === IdInput THEN appendChild
+    // let ListItemById = document.createElement('LI')
+    // listItemById.innerText = arrayOfTodos[i].title
+    // list.appendChild(newListItem)
+
     // for (let i = 0; i < arrayOfTodos.length; i++) {
     //     let filteredListItem = document.createElement('LI')
     //     filteredListItem.innerText = arrayOfTodos[i].title
     //     list.appendChild(filteredListItem)
     // }
-    
-}
-
-const populateTodos = () => {
-    for (let i = 0; i < arrayOfTodos.length; i++) {
-        let newListItem = document.createElement('LI')
-        newListItem.innerText = arrayOfTodos[i].title
-        list.appendChild(newListItem)
-    }
-}
+// }
