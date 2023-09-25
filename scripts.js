88a09a77fcfc8377415ab6e7eed03463fad3f890
daynawhite <dayna26.2@gmail.com>
2023-09-25 13:10:57 -0500

    cleaned up the code a bit

--- scripts.js
+++ scripts.js
@@ -1,9 +1,6 @@
 
 const todoList = document.getElementById('todo-list')
 
-// const userIdInput = document.getElementById('userId').value
-// console.log(userIdInput)
-
 const fetchTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos')
     .then( (response) => response.json())
@@ -12,21 +9,13 @@ const fetchTodos = () => {
 
 const logTodos = () => {
     for (let i = 0; i < arrayOfTodos.length; i++) {
-        // console.log(arrayOfTodos[i].title)
         console.log(arrayOfTodos[i])
 
     }
 
-    // console.log(arrayOfTodos)
-}
-
-// let list = document.getElementById("todo-list") 
 
 const populateTodos = () => {
     clearTodos()
-    // let listArray = array.from(li)
-    // listArray.forEach(function(elem) {
-    // })
 
     for (let i = 0; i < arrayOfTodos.length; i++) {
         let li = document.createElement('li');
@@ -48,7 +37,6 @@ const filterTodosById = () => {
        
         todoList.appendChild(li)
     }
-    // console.log(filteredTodos)
 }
 
 const filterByComplete = () => {
@@ -60,7 +48,6 @@ const filterByComplete = () => {
         if (filteredTodos[i].completed == true){
             let li = document.createElement('li');
             li.textContent = filteredTodos[i].title;
-           
             todoList.appendChild(li)
         }
     }
@@ -68,13 +55,11 @@ const filterByComplete = () => {
 const filterByIncomplete = () => {
     clearTodos()
     const userIdInput = document.getElementById('userId').value
-
     let filteredTodos = arrayOfTodos.filter((todo)=> todo.userId == userIdInput)
     for (let i = 0; i < filteredTodos.length; i++) {
         if (filteredTodos[i].completed == false){
             let li = document.createElement('li');
             li.textContent = filteredTodos[i].title;
-           
             todoList.appendChild(li)
         }
     }
@@ -83,27 +68,3 @@ const filterByIncomplete = () => {
 const clearTodos = () => {
     todoList.innerHTML = '';
 }
-// const saveIdInput = (num) => {
-//  idInput = parseInt(num) 
-// }
-
-
-// const filterTodosById = () => {
-//        todoList.innerHTML = '';
-// }
-
-
-//    const filteredList = arrayOfTodos.filter()
-//    for (let i = 0; i < arrayOfTodos.length; i++) {
-
-    // IF arrayOfTodos.userID === IdInput THEN appendChild
-    // let ListItemById = document.createElement('LI')
-    // listItemById.innerText = arrayOfTodos[i].title
-    // list.appendChild(newListItem)
-
-    // for (let i = 0; i < arrayOfTodos.length; i++) {
-    //     let filteredListItem = document.createElement('LI')
-    //     filteredListItem.innerText = arrayOfTodos[i].title
-    //     list.appendChild(filteredListItem)
-    // }
-// }
